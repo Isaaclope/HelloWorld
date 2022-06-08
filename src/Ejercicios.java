9bfdc9c5fa54a53131da2b7e24793c5d0a2c7834
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-06-08 13:51:49 +0200

    Isaac

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -1,20 +1,17 @@
 public class Ejercicios {
 
 	public static void main(String[] args) {
+		int num1=1, num2=2 ,num3=3;
+		if (num1>num2 && num1>num3) {
+			System.out.println("La variable num1 es mayor");
+		}
+		else if (num2>num3) { 
+		  	System.out.println("La variable num2 es el mayor");
+		}
+		else {
+		}
+		}
+		
 
 	}
-
-}
-
-int num1=1, num2=2 ,num3=3;
-if (num1>num2 && num1>num3) {
-	System.out.printl("La variable num1 es mayor");
-}
-else if ("La variable num2 es el mayor");
-}
-else {
-
-
-
-
 
